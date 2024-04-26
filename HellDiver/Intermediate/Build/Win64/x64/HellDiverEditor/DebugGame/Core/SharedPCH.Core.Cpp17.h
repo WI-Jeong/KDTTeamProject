69b3c2d453bfac67f6a0a8d5fcdf3bb7be906e84
DESKTOP-COA1C72\AR student <test@gmail.com>
2024-04-26 17:01:13 +0900

    파일 삭제

--- HellDiver/Intermediate/Build/Win64/x64/HellDiverEditor/DebugGame/Core/SharedPCH.Core.Cpp17.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Core/Public/CoreSharedPCH.h
-#include "D:/Han/bira/KDTUnreal/HellDiver/HellDiver/Intermediate/Build/Win64/x64/HellDiverEditor/DebugGame/Core/SharedDefinitions.Core.Cpp17.h"
-#include "Runtime/Core/Public/CoreSharedPCH.h"
--- HellDiver/Source/HellDiver/HellDiver.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "HellDiver.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HellDiver, "HellDiver" );
- 
