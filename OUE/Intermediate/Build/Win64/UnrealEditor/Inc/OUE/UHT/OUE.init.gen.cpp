0bc64c86e6735ab2d40970f150e3fabd6d814c75
DESKTOP-COA1C72\AR student <test@gmail.com>
2024-04-30 18:03:14 +0900

    item 작업중

--- OUE/Intermediate/Build/Win64/UnrealEditor/Inc/OUE/UHT/OUE.init.gen.cpp
+++ OUE/Intermediate/Build/Win64/UnrealEditor/Inc/OUE/UHT/OUE.init.gen.cpp
@@ -17,13 +17,13 @@ void EmptyLinkFunctionForGeneratedCodeOUE_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x3345E4AA,
-				0x5CCE219B,
+				0x7D9C0282,
+				0xB42307FA,
 				METADATA_PARAMS(0, nullptr)
 			};
 			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_OUE.OuterSingleton, PackageParams);
 		}
 		return Z_Registration_Info_UPackage__Script_OUE.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_OUE(Z_Construct_UPackage__Script_OUE, TEXT("/Script/OUE"), Z_Registration_Info_UPackage__Script_OUE, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x3345E4AA, 0x5CCE219B));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_OUE(Z_Construct_UPackage__Script_OUE, TEXT("/Script/OUE"), Z_Registration_Info_UPackage__Script_OUE, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x7D9C0282, 0xB42307FA));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ OUE/Source/OUE/Hero/Item/Item.cpp
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Hero/Item/Item.h"
+#include "Components/SphereComponent.h"
+
+
+// Sets default values
+AItem::AItem()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComponent"));
+	SkeletalMeshComponent = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("SkeletalMeshComponent"));
+
+	SetRootComponent(SphereComponent);
+	SkeletalMeshComponent->SetupAttachment(SphereComponent);
+}
+
+// Called when the game starts or when spawned
+void AItem::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	SetItemData();
+}
+
+// Called every frame
+void AItem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	MeshRotate();
+}
+
+void AItem::MeshRotate()
+{
+	FRotator NewRotator = FRotator(0, 0, ItemDataTableRow->RotateSpeed);
+	FQuat NewQuat = NewRotator.Quaternion();
+	SetActorRelativeRotation(NewQuat);
+}
+
+void AItem::SetItemData()
+{
+	if (FItemDataTableRowHandle.IsNull()) { return; }
+	if (FItemDataTableRowHandle.RowName == NAME_None) { return; }
+
+	ItemDataTableRow = FItemDataTableRowHandle.GetRow<FItemDataTableRow>(TEXT(""));
+	if (ItemDataTableRow)
+	{
+		SkeletalMeshComponent->SetSkeletalMesh(ItemDataTableRow->USkeletalMesh);
+	}
+}
+
