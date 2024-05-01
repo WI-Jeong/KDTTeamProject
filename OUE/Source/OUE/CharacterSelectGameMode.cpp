537a331b746981dfacf3e6c4850c1a1289683dce
WI-Jeong <141189979+WI-Jeong@users.noreply.github.com>
2024-05-01 15:30:09 +0900

    캐릭터선택 레벨 (아웃라인 테스트 전 )

---
+++ OUE/Source/OUE/CharacterSelectGameMode.cpp
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CharacterSelectGameMode.h"
+#include "UI/CharacterSelectWidget.h" 
+
+ACharacterSelectGameMode::ACharacterSelectGameMode()
+{
+	DefaultPawnClass = nullptr;
+
+	static ConstructorHelpers::FClassFinder<UUserWidget>	SelectUIClass(TEXT("/Script/UMGEditor.WidgetBlueprint'/Game/JWI/UI/UI_CharacterSelect.UI_CharacterSelect_C'"));
+
+	if (SelectUIClass.Succeeded())
+		mSelectUIClass = SelectUIClass.Class;
+}
+
+void ACharacterSelectGameMode::EnableStartButton(bool Enable)
+{
+}
+
+void ACharacterSelectGameMode::InitGame(const FString& MapName, const FString& Options, FString& ErrorMessage)
+{
+	Super::InitGame(MapName, Options, ErrorMessage);
+}
+
+void ACharacterSelectGameMode::InitGameState()
+{
+	Super::InitGameState();
+}
+
+void ACharacterSelectGameMode::PostLogin(APlayerController* NewPlayer)
+{
+	Super::PostLogin(NewPlayer);
+
+	NewPlayer->SetShowMouseCursor(true);
+
+	FInputModeGameAndUI	input; 
+	NewPlayer->SetInputMode(input);
+}
+
+void ACharacterSelectGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (IsValid(mSelectUIClass))
+	{
+		mSelectWidget = CreateWidget<UCharacterSelectWidget>(GetWorld(),
+			mSelectUIClass);
+
+		if (IsValid(mSelectWidget))
+			mSelectWidget->AddToViewport();
+	}
+}
+
+void ACharacterSelectGameMode::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
