fc9bb2ec2df620e327b3c8a8fe08c3bbfa129abc
WI-Jeong <141189979+WI-Jeong@users.noreply.github.com>
2024-05-16 11:06:23 +0900

    메인UI C++코드로 변경, 미니맵 가려진것 수정

--- OUE/Source/OUE/Junglae/Controller/RPGPlayerController.h
+++ OUE/Source/OUE/Junglae/Controller/RPGPlayerController.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "../../UI/MainWidget.h"
 #include "GameFramework/PlayerController.h"
 #include "RPGPlayerController.generated.h"
 
@@ -13,7 +14,15 @@ UCLASS()
 class OUE_API ARPGPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+
+public:
+	ARPGPlayerController();
 	
 public:
 	virtual void OnPossess(APawn* PawnToPossess) override;
+	virtual void BeginPlay() override;
+
+protected:
+	TSubclassOf<UUserWidget> MainHUDWidgetClass;
+	UMainWidget* MainWidget;
 };
