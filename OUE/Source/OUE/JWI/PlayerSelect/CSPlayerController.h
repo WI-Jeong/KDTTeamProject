32e6f7c9423c8fc244cc2d4bb0a2ae077b55e55f
WI-Jeong <141189979+WI-Jeong@users.noreply.github.com>
2024-05-17 19:51:20 +0900

    캐릭터 선택 레벨 작업 중

---
+++ OUE/Source/OUE/JWI/PlayerSelect/CSPlayerController.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "CSPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OUE_API ACSPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	ACSPlayerController();
+
+protected:
+	TObjectPtr<AActor> mCursorPickActor;
+
+protected:
+	virtual void OnConstruction(const FTransform& Transform);
+	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason);
+	virtual void OnPossess(APawn* InPawn) override;
+	virtual void OnUnPossess() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	void ChangePickOutLine(int32 StencilValue);
+
+};
